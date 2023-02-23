4fb5ba1d2a668a3f80e638d5572716519e37e99a
Matt Fleming <122564640+chartpro-com@users.noreply.github.com>
2023-02-23 16:09:25 -0500

    ironed out past, present, future and AM, PM

--- Develop/script.js
+++ Develop/script.js
@@ -29,47 +29,43 @@ $("#currentDay").text(currentDate);
 function startCalendar(){
 
   // for i of the hour with an dayjs > then whenTime = past
-var workHours = ["8","12"]
-var whenTime = ""
-var displayTime = ""
-var currentHour = dayjs().hour();
-console.log(currentHour)
+  var workHours = ["9","10","11","12","13","14","15","16","17"]
+  var whenTime = ""
+  var currentHour = dayjs().hour();
+  var displayTime
+  // console.log(currentHour)
 
   // Add time blocks to the HTML
-  for (var i = 0; i < workHours.length; i++) {
+    for (var i = 0; i < workHours.length; i++) {
 
-    if (currentHour > workHours[i]) {
-      console.log("hi");
-    };
-
-    tempHours = workHours[i]
-    if (tempHours === "11") {
-      whenTime = "past"
-      displayTime = workHours[i]
-      // console.log(displayTime)
-    } else {
-      whenTime = "present"
-      displayTime = workHours[i]
+      if (currentHour == workHours[i]) {
+        whenTime = "present"
+      } else if (currentHour < workHours[i]){
+        whenTime = "future"
+      } else {
+        whenTime = "past"
+      }
+      if (workHours[i] > 12) {
+        displayTime = (workHours[i] - 12) + "PM"
+      } else {
+        displayTime = workHours[i] + "AM"
+      }
+      var timeBlockEl = $("<div>").addClass("row time-block");
+      timeBlockEl.addClass(whenTime);
+      timeBlockEl.attr("id", displayTime);
+      var hourEl = $("<div>")
+        .addClass("col-2 col-md-1 hour text-center py-3")
+        .text(displayTime);
+      var descriptionEl = $("<textarea>")
+        .addClass("col-8 col-md-10 description")
+        .attr("rows", 3);
+      var saveBtnEl = $("<button>")
+        .addClass("btn saveBtn col-2 col-md-1")
+        .attr("aria-label", "save")
+        .html("<i class='fas fa-save' aria-hidden='true'></i>");
+      timeBlockEl.append(hourEl, descriptionEl, saveBtnEl);
+      $(".container-fluid").append(timeBlockEl);
     }
-
-
-    var timeBlockEl = $("<div>").addClass("row time-block");
-    timeBlockEl.addClass(whenTime);
-    timeBlockEl.attr("id", displayTime);
-    var hourEl = $("<div>")
-      .addClass("col-2 col-md-1 hour text-center py-3")
-      .text(displayTime);
-    var descriptionEl = $("<textarea>")
-      .addClass("col-8 col-md-10 description")
-      .attr("rows", 3);
-      // console.log("rows", 3);
-    var saveBtnEl = $("<button>")
-      .addClass("btn saveBtn col-2 col-md-1")
-      .attr("aria-label", "save")
-      .html("<i class='fas fa-save' aria-hidden='true'></i>");
-    timeBlockEl.append(hourEl, descriptionEl, saveBtnEl);
-    $(".container-fluid").append(timeBlockEl);
-  }
   
 }
   
